89f5255103fa18737665d3f8f1319212770e09fa
objlynks <obedjonathan26@gmail.com>
2023-01-31 15:16:25 +0100

    Added get employee by ID rest Api

--- src/main/java/com/objlynks/springboot/controller/EmployeeController.java
+++ src/main/java/com/objlynks/springboot/controller/EmployeeController.java
@@ -3,15 +3,19 @@ package com.objlynks.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.objlynks.springboot.exception.ResourceNotFoundException;
 import com.objlynks.springboot.model.Employee;
 import com.objlynks.springboot.repository.EmployeeRepository;
+
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -34,4 +38,13 @@ public class EmployeeController {
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
+	
+	//get employee by ID rest API
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+		return ResponseEntity.ok(employee);
+	}
+	
 }
