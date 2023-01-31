3a472ccae03d2540fc73093785a75572e3615a67
objlynks <obedjonathan26@gmail.com>
2023-01-31 01:50:20 +0100

    Added "Create Employee Rest API"

--- src/main/java/com/objlynks/springboot/controller/EmployeeController.java
+++ src/main/java/com/objlynks/springboot/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package com.objlynks.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +20,17 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository  employeeRepository;
 
-	//get all employees
-	
+	//get all employees Rest API
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
 		
+	}
+	
+	//create Employee Rest API
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
