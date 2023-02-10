e9242146cfed7897791de9687384da1abe2d7726
objlynks <obedjonathan26@gmail.com>
2023-02-10 15:36:05 +0100

    updated

--- src/main/java/com/objlynks/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/objlynks/springboot/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.objlynks.springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.objlynks.springboot.model.Employee;
 
 @Repository
